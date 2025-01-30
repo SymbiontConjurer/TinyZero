73197ce11a6870536ccb2ca440e7628f619662bd
SymbiontConjurer <135414057+SymbiontConjurer@users.noreply.github.com>
2025-01-30 12:37:09 -0800

    Update project names

--- examples/grpo_trainer/run_qwen2-7b.sh
+++ examples/grpo_trainer/run_qwen2-7b.sh
@@ -32,8 +32,8 @@ python3 -m verl.trainer.main_ppo \
     algorithm.kl_ctrl.kl_coef=0.001 \
     trainer.critic_warmup=0 \
     trainer.logger=['console','wandb'] \
-    trainer.project_name='verl_grpo_example_gsm8k' \
-    trainer.experiment_name='qwen2_7b_function_rm' \
+    trainer.project_name='TinyZero' \
+    trainer.experiment_name='example-qwen2_7b_function_rm' \
     trainer.n_gpus_per_node=8 \
     trainer.nnodes=1 \
     trainer.save_freq=-1 \
--- scripts/grpo/train_qwen_3b_grpo.sh
+++ scripts/grpo/train_qwen_3b_grpo.sh
@@ -32,7 +32,7 @@ python3 -m verl.trainer.main_ppo \
     algorithm.kl_ctrl.kl_coef=0.001 \
     trainer.critic_warmup=0 \
     trainer.logger=['console','wandb'] \
-    trainer.project_name='verl_grpo_example_gsm8k' \
+    trainer.project_name='TinyZero' \
     trainer.experiment_name='qwen2.5-3b-grpo' \
     trainer.n_gpus_per_node=8 \
     trainer.nnodes=1 \
